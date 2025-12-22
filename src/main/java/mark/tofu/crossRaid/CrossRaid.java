ec68d885e1dfdc634bb9d715cdc140e7cb9250dc
akiaki1122 <akiakiyop1122@gamil.com>
2025-12-22 17:52:06 +0900

    first commit

---
+++ src/main/java/mark/tofu/crossRaid/CrossRaid.java
@@ -0,0 +1,17 @@
+package mark.tofu.crossRaid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrossRaid extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
